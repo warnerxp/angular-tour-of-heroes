7a3c0e68204326826074e8b037330803c3b70894
LL\wagalvar <wagner.alvarado@publicissapient.com>
2022-07-04 15:04:33 -0600

    Big Commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes: Hero[] | undefined;
+  
+   /**
+  selectedHero: Hero | undefined;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+   */
+  constructor(private heroService: HeroService,private messageService: MessageService) { }
+  
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+
+}
